7ccaeac73e50bf4a017b632756c8ff2728ded47a
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2024-11-28 22:15:03 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/jvrviegas/gopportunities/router"
+import (
+	"github.com/jvrviegas/gopportunities/config"
+	"github.com/jvrviegas/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize router
 	router.Initialize()
 }
