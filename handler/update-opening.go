278a4dea8805beb82e042322f68e45ed71b83a42
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2024-12-04 21:49:39 -0300

    docs: add documentation for update opening

--- handler/update-opening.go
+++ handler/update-opening.go
@@ -7,6 +7,20 @@ import (
 	"github.com/jvrviegas/gopportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Update opening
+// @Description Update an existing job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening ID"
+// @Param request body UpdateOpeningRequest true "Request body"
+// @Success 200 {object} UpdateOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [put]
 func UpdateOpeningHandler(ctx *gin.Context) {
 	request := UpdateOpeningRequest{}
 
